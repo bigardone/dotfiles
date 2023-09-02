0bab1b78be9ba0364c49bf717a61b4b0bbd0639d
Ricardo Garcia Vega <bigardone@gmail.com>
2023-09-02 08:26:59 +0200

    minor changes

--- .config/nvim/lua/treesitter-config/init.lua
+++ .config/nvim/lua/treesitter-config/init.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  -- ignore_install = { "elixir" },
+  ignore_install = { "po" },
   highlight = {
     enable = true, -- false will disable the whole extension
     additional_vim_regex_highlighting = false,
